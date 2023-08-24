5e70acf3f036d62d8879574290645d0a74079700
孙林耀 <sunlinyao@vip.qq.com>
2023-08-24 14:04:32 +0800

    optimize: code
    Signed-off-by: 孙林耀 <sunlinyao@vip.qq.com>

---
+++ scripts/start.sh
@@ -0,0 +1,16 @@
+cd $(dirname $0)/..
+
+isRunning=$(docker inspect openldap |jq ".[0].State.Running")
+if [ "$isRunning" == "false" ];then
+    docker start openldap
+fi
+make protos idas && (
+    cd dist
+    ./idas \
+      --log.level=debug \
+      --log.format=idas \
+      --http.openapi-path=/apidocs.json \
+      --http.openapi-path=/apidocs.json \
+      --swagger.file-path=./swagger-ui \
+      --http.external-url=http://192.168.122.1:8000/
+)
