25d2aa1e1146d67c4b4bed3893a17f1522c012ca
孙林耀 <sunlinyao@vip.qq.com>
2024-09-23 18:27:26 +0800

    optimize: i18n utils
    Signed-off-by: 孙林耀 <sunlinyao@vip.qq.com>

--- public/src/utils/intl.ts
+++ public/src/utils/intl.ts
@@ -1,22 +1,66 @@
 import type { PrimitiveType } from 'intl-messageformat';
-import type { IntlShape, MessageDescriptor } from 'react-intl';
+import type { IntlShape, MessageDescriptor, IntlFormatters, FormatDateOptions, FormatNumberOptions, FormatPluralOptions, FormatRelativeTimeOptions } from 'react-intl';
 
 import locales from '@/locales/zh-CN';
+import { DisplayNamesOptions } from '@formatjs/intl-displaynames/lib';
+import { IntlListFormatOptions } from '@formatjs/intl-listformat';
+import { FormattableUnit } from '@formatjs/intl-relativetimeformat';
+import { ReactNode } from 'react';
 
-export class IntlContext {
+export class IntlContext implements IntlFormatters {
   private id: string;
   private parent: IntlContext | IntlShape;
+  locale: string;
   constructor(id: string, parent: IntlContext | IntlShape) {
     this.id = id;
     this.parent = parent;
     this.locale = parent.locale;
   }
-  locale: string;
+  formatDate(value: string | number | Date | undefined, opts?: FormatDateOptions | undefined): string {
+    return this.parent.formatDate(value, opts);
+  }
+  formatTime(value: string | number | Date | undefined, opts?: FormatDateOptions | undefined): string {
+    return this.parent.formatTime(value, opts);
+  }
+  formatDateToParts(value: string | number | Date | undefined, opts?: FormatDateOptions | undefined): Intl.DateTimeFormatPart[] {
+    return this.parent.formatDateToParts(value, opts);
+  }
+  formatTimeToParts(value: string | number | Date | undefined, opts?: FormatDateOptions | undefined): Intl.DateTimeFormatPart[] {
+    return this.parent.formatTimeToParts(value, opts);
+  }
+  formatRelativeTime(value: number, unit?: FormattableUnit | undefined, opts?: FormatRelativeTimeOptions | undefined): string {
+    return this.parent.formatRelativeTime(value, unit, opts);
+  }
+  formatNumber(value: number | bigint, opts?: FormatNumberOptions | undefined): string {
+    return this.parent.formatNumber(value, opts);
+  }
+  formatNumberToParts(value: number | bigint, opts?: FormatNumberOptions | undefined): Intl.NumberFormatPart[] {
+    return this.parent.formatNumberToParts(value, opts);
+  }
+  formatPlural(value: number, opts?: FormatPluralOptions | undefined): Intl.LDMLPluralRule {
+    return this.parent.formatPlural(value, opts);
+  }
+  formatList(values: string[], opts?: IntlListFormatOptions | undefined): string;
+  formatList(values: ReactNode[], opts?: IntlListFormatOptions | undefined): React.ReactNode;
+  formatList(values: ReactNode[] | string[], opts?: IntlListFormatOptions | undefined): React.ReactNode {
+    return this.parent.formatList(values, opts);
+  }
+  formatDisplayName(value: string | number | object, opts?: DisplayNamesOptions | undefined): string | undefined {
+    return this.parent.formatDisplayName(value, opts);
+  }
+  formatHTMLMessage(descriptor: MessageDescriptor, values?: Record<string, PrimitiveType>): React.ReactNode {
+    const id = this.getId(descriptor.id);
+    if (!(this.parent instanceof IntlContext) && id) {
+      if (!(locales as Record<string, string>)[id])
+        console.log(`'${id}': '${descriptor.defaultMessage}',`);
+    }
+    return this.parent.formatHTMLMessage({ ...descriptor, id: id }, values);
+  }
   formatMessage(descriptor: MessageDescriptor, values?: Record<string, PrimitiveType>): string {
     const id = this.getId(descriptor.id);
     if (!(this.parent instanceof IntlContext) && id) {
       if (!(locales as Record<string, string>)[id])
-        console.log(`"${id}": "${descriptor.defaultMessage}",`);
+        console.log(`'${id}': '${descriptor.defaultMessage}',`);
     }
     return this.parent.formatMessage({ ...descriptor, id: id }, values);
   }
