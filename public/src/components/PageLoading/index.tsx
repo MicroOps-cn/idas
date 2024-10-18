e3f40485eb55cbec19acaf3c7341b49a272b9526
MicroOps <sunlinyao@vip.qq.com>
2024-10-18 13:26:41 +0800

    update: antd to v5
    Signed-off-by: MicroOps <sunlinyao@vip.qq.com>

---
+++ public/src/components/PageLoading/index.tsx
@@ -0,0 +1,12 @@
+const PageLoading: React.FC = () => {
+  return (
+    <div className="center-body">
+      <div className="loader-circle-9">
+        Loading
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default PageLoading;
